6e561f9b9322b2b542f74cf84dbc3511d03232e3
Bailey Segall <bailey@segallgirls.com>
2021-02-23 12:30:30 -0800

    color add

--- memoryeyetest/sketch.js
+++ memoryeyetest/sketch.js
@@ -1,19 +1,13 @@
 
-//easy -->db9c95
-//medium-->d9a29c
-//hard --> dbaca7
-//hardest --> dbb6b2
-
 let squares=[];
 let nums=[];
 let found=0;
 let level=0;
 let clicked=0;
+let c='#db9c95';
+
+let speed= 500/(1+(0.25*level));
  
-//easy -->db9c95
-//medium-->d9a29c
-//hard --> dbaca7
-//hardest --> dbb6b2
  
 function setup() {
  createCanvas(windowWidth,windowHeight);
@@ -31,11 +25,48 @@ function setup() {
    }
  }
 }
+
+function cls(){
+  if (level>100){
+    return '#E9C4BF';
+  }
+  else if(level>90){
+    return '#E8BFBA';
+  }
+  else if(level>80){
+    return '#E6BAB5';
+  }
+  else if(level>70){
+    return '#E4B5B0';
+  }
+  else if(level>60){
+    return '#E2B0AA';
+  }
+  else if(level>50){
+    return '#E0ABA5';
+  }
+  else if(level>40){
+    return '#DFA6A0';
+  }
+  else if(level>30){
+    return '#DDA19A';
+  }
+  else if(level>20){
+    return '#D0948E';
+  }
+  else if(level>10){
+    return '#C58C86';
+  }
+  else{
+    return '#BA857F';
+  }
+}
  
 function draw() {
  //background(50, 89, 100);
  for (let i = 0; i < squares.length; i++) {
    squares[i].display();}
+  
 }
  
 function keyPressed(){
@@ -57,15 +88,12 @@ function flipping(){
      append(nums,temp);}
  }
  nums.forEach(myfunc)
- console.log(nums);
 }
  
 function myfunc(num){
- console.log(num);
- squares[num].color=color('#c59893');
+ squares[num].color=color(cls());
  squares[num].flipped=true;
- console.log(squares[num].flipped);
- setTimeout(() => {squares[num].color=color('#dbb8b4');}, 500)
+ setTimeout(() => {squares[num].color=color('#EBC9C5');}, speed)
 }
  
 class square{
@@ -73,7 +101,7 @@ class square{
    this.x=a;
    this.y=b;
    this.flipped=false;
-   this.color=color('#dbb8b4');
+   this.color='#EBC9C5';
  }
   display(){
    //what the sqaure looks like
@@ -108,7 +136,7 @@ function mouseClicked(){
  
   for (i=0; i<squares.length; i++){
    if(isInRectangleArray(x,y,squares[i])){
-     squares[i].color=color('#c59893');
+     squares[i].color=color(cls());
      found+=1;
    }
     if(found==nums.length && found>0){
@@ -129,9 +157,10 @@ function mouseClicked(){
  
 function reset(){
  for(i=0; i<nums.length;i++){
-   squares[nums[i]].color=color('#dbb8b4');
+   squares[nums[i]].color=color('#EBC9C5');
    squares[nums[i]].flipped=false;
  }
  nums=[];
  found=0;
  }
+
