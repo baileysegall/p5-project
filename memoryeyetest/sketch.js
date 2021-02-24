3de86834f6b1b4ddc0032b46cc7d2b0cb1fdee66
Bailey Segall <bailey@segallgirls.com>
2021-02-23 19:59:42 -0800

    redone

--- memoryeyetest/sketch.js
+++ memoryeyetest/sketch.js
@@ -26,7 +26,6 @@ function setup() {
      squares.push(new square((windowWidth-430)/2+x,(windowHeight-390)/2+y));
    }
  }
- text('click to begin each round',30, windowHeight/2)
 }
 
 function cls(){
